76e69e5fff39a1e8e77b7e2c40b4c7d7afc4ad68
Yang Sun <sunyang@sunyangdeMacBook-Pro.local>
2017-03-07 23:59:50 -0600

    import data to the index page

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+def get_csv():
+	csv_path = "static/la-riots-deaths.csv"
+	csv_file = open(csv_path, "rb")
+	# rb means 'Read the file in Binary mode'
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
+
+
+
+
 @app.route("/")
 def index():
 	template = "index.html"
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list = object_list)
 
 if __name__ == "__main__":
 	#Fire up the Flask test server
